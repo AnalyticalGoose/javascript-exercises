ad821cad6b1722a4587c79c50ea784e6be172eac
Harry <harryedgar@hotmail.co.uk>
2023-05-12 09:33:46 +0100

    complete exercies 3-5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0 || !Number.isInteger(x && y)) {
+        return 'ERROR'
+    }
+    else {
+        x > y ? [x, y] = [y, x] : false
+        return y*(x + y)/2
+    }
 };
 
 // Do not edit below this line
